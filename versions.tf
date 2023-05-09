d0b2fefd8ced10af02c0e647f19131fc94efbdea
Caleb Woodbine <c.woodbine@gns.cri.nz>
2023-05-10 11:14:22 +1200

    Add null provider

--- versions.tf
+++ versions.tf
@@ -1,5 +1,11 @@
 terraform {
   required_version = ">=1.4.0"
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.1"
+    }
+  }
 
   cloud {
     organization = "calebwoodbine"
